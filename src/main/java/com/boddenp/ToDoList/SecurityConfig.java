310c877ab626cf9ed38f35ad08f78d9de4668242
aboddenp <aboddenp@github.com>
2021-04-30 10:01:09 -0600

    Post Forbbiden fix

--- src/main/java/com/boddenp/ToDoList/SecurityConfig.java
+++ src/main/java/com/boddenp/ToDoList/SecurityConfig.java
@@ -4,15 +4,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     protected void configure(HttpSecurity security) throws Exception {
         security
+                .csrf().disable()
                 .httpBasic().disable()
                 .formLogin().disable();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
